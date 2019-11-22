b0d293d8c96bf264f4272955c0d1005e775c6907
xdeepanshu <deepanshusinghrautela@gmail.com>
2019-11-22 20:19:31 +0530

    Fix URL regex

--- todo/urls.py
+++ todo/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^search/$', search_views.search, name='search'),
-    url(r'^api/$', include('api.urls')),
+    url(r'^api/', include('api.urls')),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
