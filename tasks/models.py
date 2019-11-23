4f1fa5585be244cbdcbc861de5cd608f76d533e5
xdeepanshu <deepanshusinghrautela@gmail.com>
2019-11-23 16:00:33 +0530

    Fix default hex value to white

--- tasks/models.py
+++ tasks/models.py
@@ -5,7 +5,7 @@ class Task(models.Model):
     finish_time = models.DateTimeField(auto_now=False, auto_now_add=False)
     created_time = models.DateTimeField(auto_now=True, editable=False)
     completed = models.BooleanField(default=False)
-    hex_color = models.CharField(max_length=10, default='#255255255')
+    hex_color = models.CharField(max_length=10, default="#FFFFFF")
 
     def __str__(self):
         return self.task_description
