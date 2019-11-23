f69787eacdc134bf7b6f9cd2c6cb4ca51a725dab
xdeepanshu <deepanshusinghrautela@gmail.com>
2019-11-23 15:57:57 +0530

    Add hex color

--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from tasks.models import Task
 class TasksSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ("id","task_description", "finish_time", "completed", "created_time")
+        fields = ("id","task_description", "finish_time", "completed", "created_time", "hex_color")
