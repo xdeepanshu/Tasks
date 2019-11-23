c96d6396d4a0bf6022d4de36e490d89686bfb4f5
xdeepanshu <deepanshusinghrautela@gmail.com>
2019-11-23 16:16:47 +0530

    Fix Swagger error when hosting on AWS

--- todo/settings/base.py
+++ todo/settings/base.py
@@ -167,3 +167,7 @@ WAGTAIL_SITE_NAME = "todo"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+
+#Had to add this to make swagger work
+REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
